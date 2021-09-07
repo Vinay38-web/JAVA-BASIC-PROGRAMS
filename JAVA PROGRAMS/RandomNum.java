1d24cc05aa4b5f17b4e7626d1ac0ceccae20586f
EPURI VENKATA VINAY <87356064+Vinay38-web@users.noreply.github.com>
2021-09-07 21:52:05 +0530

    Add files via upload

---
+++ JAVA PROGRAMS/RandomNum.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class RandomNum
+{
+	public static void main(String [] args)
+	{
+		Random rand=new Random();
+		int rand1=rand.nextInt();
+		System.out.println(rand1);
+	}
+}
